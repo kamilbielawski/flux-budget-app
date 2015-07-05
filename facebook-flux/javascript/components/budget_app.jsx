24e2238a4b14e927975ed9b8298297b683dc5e0f
Kamil Bielawski <k_bielawski@fastmail.fm>
2015-07-06 01:41:22 +0200

    Initial commit
    * Project structure
    * Facebook-flux Hello World

---
+++ facebook-flux/javascript/components/budget_app.jsx
@@ -0,0 +1,15 @@
+'use strict';
+
+var React = require('react');
+
+var BudgetApp = React.createClass({
+  render: function () {
+    return (
+      <div>
+        Hello World!
+      </div>
+    );
+  }
+});
+
+module.exports = BudgetApp;
